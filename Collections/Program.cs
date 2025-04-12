b9189fbb392e089f8a2c081b999f20f7a0fb7209
vladkop2025 <vladkop@mail.ru>
2025-04-12 21:30:51 +0300

    unit3_2

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace Collections
+namespace PhoneBook
 {
     class Program
     {
+        //  Объявим  сортированный  словарь
+        private static SortedDictionary<string, Contact> SortedPhoneBook = new SortedDictionary<String, Contact>()
+        {
+            ["Игорь"] = new Contact(79990000000, "igor@example.com"),
+            ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
+        };
+
         static void Main(string[] args)
         {
+            // Запустим таймер
+            var watchTwo = Stopwatch.StartNew();
+
+            // Выполним вставку
+            SortedPhoneBook.Add("Диана", new Contact(79160000002, "diana@example.com"));
+
+            // Выведем результат
+            Console.WriteLine($"Вставка в сортированный словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+        }
+        public class Contact // модель класса
+        {
+            public Contact(long phoneNumber, String email) // метод-конструктор
+            {
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+            public long PhoneNumber { get; set; }
+            public String Email { get; set; }
         }
     }
 }
+
+//Вставка в сортированный словарь: 0,0195  мс
