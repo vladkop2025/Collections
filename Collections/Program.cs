df720e91cdf58614b477906597c43b2cd196289a
vladkop2025 <vladkop@mail.ru>
2025-04-13 21:44:06 +0300

    гтше4

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Collections
+namespace LinkedListTest
 {
     class Program
     {
-        static void Main(string[] args)
+        // объявим список в виде статической переменной
+        public static LinkedList<string> LinkedList = new LinkedList<string>();
+
+        static void Main()
         {
+
+            //// читаем весь файл с рабочего стола в строку текста
+            //string text = File.ReadAllText("C:\\Users\\vladk\\Desktop\\cdev_Text.txt");
+
+            ////убираем знаки пунктуации из текста
+            //var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            //// Сохраняем символы-разделители в массив
+            //char[] delimiters = new char[] { ' ', '\r', '\n' };
+
+            //// разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            //var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
         }
     }
 }
