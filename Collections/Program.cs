865e8ea28804e147edd903cbeb99d75010e7e407
vladkop2025 <vladkop@mail.ru>
2025-04-13 19:47:14 +0300

    unit3_3_1

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,48 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace StackTest
+namespace QueueTest
 {
     class Program
     {
-        public static Stack<string> words = new Stack<string>();
-
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в стек.");
-            Console.WriteLine();
+            // создаем очередь
+            Queue<int> q = new Queue<int>();
 
-            while (true)
+            // добавляем в нее целые числа от 0 до 10
+            for (int i = 0; i <= 10; i++)
             {
-                var input = Console.ReadLine();
-
-                switch (input)
-                {
-                    //   если  команда pop - пробуем достать элемент
-                    case "pop":
-                        words.TryPop(out string popResult);
-                        break;
-                    //   если  команда peek - пробуем  просмотреть элемент
-                    case "peek":
-                        words.TryPeek(out string peekResult);
-                        break;
-                    default:
-                        // если ни одна из команд не распознана - простов стек добавляем то что введено
-                        words.Push(input);
-                        break;
-                }
-
-
-                Console.WriteLine();
-                Console.WriteLine("В стеке:");
-
-                
-                foreach (var word in words)
-                {
-                    Console.WriteLine(" " + word);
-                }
+                q.Enqueue(i);
+                Console.WriteLine($"{i} зашёл в очередь");
             }
+
+            // Посмотрим, кто первый в очереди
+            Console.WriteLine($"Первый элемент:   {q.Peek()}");
+
+            // Обратите внимание, после  вызова Peek() элемент остается в очереди
+
+            //  Посмотрим всю очередь
+            Console.WriteLine("Элементы в очереди");
+            foreach (int i in q)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+            Console.WriteLine($"В очереди  {q.Count} элементов");
+            // обработаем очередь -
+            // достанем из неё элементы один за другим
+            var queueLength = q.Count;
+            for (int i = 0; i < queueLength; i++)
+                Console.WriteLine($"{q.Dequeue()} вышел из очереди");
+            //  Посмотрим, сколько элементов осталось
+            Console.WriteLine($"В очереди  {q.Count} элементов");
         }
     }
 }
-
