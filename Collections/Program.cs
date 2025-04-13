f9b8192b563ed1c46fdea9f0fbeaffd0b3ce04fa
vladkop2025 <vladkop@mail.ru>
2025-04-13 19:39:09 +0300

    unit3_3

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Collections
+namespace StackTest
 {
     class Program
     {
+        public static Stack<string> words = new Stack<string>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в стек.");
+            Console.WriteLine();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    //   если  команда pop - пробуем достать элемент
+                    case "pop":
+                        words.TryPop(out string popResult);
+                        break;
+                    //   если  команда peek - пробуем  просмотреть элемент
+                    case "peek":
+                        words.TryPeek(out string peekResult);
+                        break;
+                    default:
+                        // если ни одна из команд не распознана - простов стек добавляем то что введено
+                        words.Push(input);
+                        break;
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine("В стеке:");
+
+                
+                foreach (var word in words)
+                {
+                    Console.WriteLine(" " + word);
+                }
+            }
         }
     }
 }
+
